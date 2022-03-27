431237bdfbf90199e8a5128be34ba99636308c80
Krzysztof <swiatlyk@gmail.com>
2022-03-27 17:24:42 +0200

    Added new activity

--- app/src/main/java/com/example/coolresume/MainActivity.kt
+++ app/src/main/java/com/example/coolresume/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.coolresume
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val workHistory = findViewById<Button>(R.id.workHistory)
+
+        workHistory.setOnClickListener {
+
+            val moveToWorkHistory = Intent(getApplicationContext(), WorkHistoryActivity::class.java)
+            startActivity(moveToWorkHistory)
+        }
     }
 }
